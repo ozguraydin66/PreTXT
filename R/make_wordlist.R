16333b0ef488f10ca33cd0ec93ec699c9e986f06
Özgür Aydın <34584376+ozguraydin66@users.noreply.github.com>
2025-10-19 23:29:02 +0300

    Update make_wordlist.R

--- R/make_wordlist.R
+++ R/make_wordlist.R
@@ -73,3 +73,16 @@ make_wordlist <- function(text.path,pgraph,title=FALSE,bigram.type='mean',lang='
   }
   return(list(df,WordList))
 }
+#' Data for PreTXT
+#'
+#' @format A data frame with 29414 rows and 3 columns.
+#' describe{
+#' item{Character}{Single value - words}
+#' item{Character}{TSyllable of the word}
+#' item{Numeric}{Frequencies}
+#' }
+#' @source Created by Keuleers, E., & Brysbaert, M. (2010) [Wuggy]
+#'
+#' @examples
+#' data(turkish)
+"turkish"
